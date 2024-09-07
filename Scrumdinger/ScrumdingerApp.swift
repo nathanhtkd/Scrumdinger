e7aade7a0564c30d219f5c352491756ad46393ba
Nathan Hartojo <nathanhartojo@gmail.com>
2024-09-07 00:51:42 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nathan Hartojo on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
